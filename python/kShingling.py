011fcc77ae861971c2a89fdb56ac8be1fe18ec29
Anand Tyagi <anand.deep.tyagi@gmail.com>
2023-10-25 13:43:24 -0400

    create sparce vectors

--- python/kShingling.py
+++ python/kShingling.py
@@ -1,11 +1,32 @@
 def kShingling(sentences, k=2):
-    k_shingles = []
+    vocab = []
+    all_shingles = []
     for s in sentences:
+        shingles = []
+
         for i in range(0, len(s) - k + 1):
 
-            k_shingles.append(s[i:i+k])
-        
-    return set(k_shingles)
+            shingles.append(s[i:i+k])
+        vocab += shingles
+        all_shingles = (s, set(shingles))
 
-testShingle = kShingling(["this is a test sentence"])
-print(testShingle)
+    return all_shingles, set(vocab)
+
+def createSparseVectors(shingles, vocab):
+    zeroVector = [0] * len(vocab)
+    sparseVectors = []
+    for shingle in shingles:
+        oneHot = zeroVector.copy()
+        for i, v in enumerate(vocab):
+            if v in shingle:
+                oneHot[i] = 1
+        sparseVectors.append(oneHot)
+
+    return sparseVectors
+
+test_sentences = ["this is a test sentence", "this is another test sentence", "sentence number three!"]
+testShingles, vocab = kShingling(test_sentences)
+sparceVectors = createSparseVectors(testShingles, vocab)
+
+
+
